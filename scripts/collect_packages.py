832bb03d65b417b514f8ffa7bbbc01626fd5cfa1
Natasha Singh <dukedesi22@gmail.com>
2022-11-29 21:46:38 -0500

    :construction_worker: Run CI jobs for multiple ingest pkgs in parallel

--- scripts/collect_packages.py
+++ scripts/collect_packages.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python
 
 import os
+import json
 
 import yaml
 from yaml.loader import SafeLoader
@@ -12,20 +14,23 @@ PACKAGES_DIR = "d3b_ingest_packages/packages"
 
 def collect_packages():
     """
-    Read relative ingest package paths from run.yaml and output
-    paths that are relative to __ROOT_DIR__
+    Read relative ingest package paths from run.yaml
+    Check to make sure each path exists
+    Output list of package paths
     """
     pass
 
     with open(RUN_FILEPATH) as f:
         data = yaml.load(f, Loader=SafeLoader)
 
+        paths = []
         for p in data["packages"]:
             new_path = os.path.join(PACKAGES_DIR, p)
             # Check if path exists
             os.stat(new_path)
             # Write relative ingest package path to stdout
-            print(new_path)
+            paths.append(p)
+        print(json.dumps(paths))
 
 
 if __name__ == "__main__":
