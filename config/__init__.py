9c934cd0e0952ef023343c0b6f9cf1c0b4ba54a4
Natasha Singh <dukedesi22@gmail.com>
2022-12-15 14:30:56 -0500

    :truck: Put all config stuff in own package

---
+++ config/__init__.py
@@ -0,0 +1,22 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.dirname((__file__)))
+PACKAGES_DIR = "d3b_ingest_packages/packages"
+
+INGEST_DB_HOST = os.getenv("FLY_POSTGRES_INGEST_DB_HOST")
+INGEST_DB_PORT = os.getenv("FLY_POSTGRES_INGEST_DB_PORT")
+INGEST_PROCESS_USER = os.getenv("FLY_POSTGRES_INGEST_USER")
+INGEST_PROCESS_PASSWORD = os.getenv("FLY_POSTGRES_INGEST_PASSWORD")
+INGEST_VIEWER_USER = os.getenv("FLY_POSTGRES_VIEWER_USER")
+INGEST_VIEWER_PASSWORD = os.getenv("FLY_POSTGRES_VIEWER_PASSWORD")
+
+METABASE_DB_USER = os.getenv("MB_DB_USER")
+METABASE_DB_PASSWORD = os.getenv("MB_DB_PASS")
+METABASE_APP_ADMIN = os.getenv("MB_APP_ADMIN")
+METABASE_APP_ADMIN_EMAIL = os.getenv("MB_APP_ADMIN_EMAIL")
+METABASE_APP_ADMIN_PASSWORD = os.getenv("MB_APP_ADMIN_PASS")
+METABASE_APP_URL = os.getenv("MB_APP_URL")
+
+METABASE_SETUP_TEMPLATE = os.path.join(
+    ROOT_DIR, "config", "metabase_setup_template.json"
+)
