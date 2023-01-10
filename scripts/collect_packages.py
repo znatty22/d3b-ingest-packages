d06bd8e9e648366a06c326ce3ff73c79c71b34ff
Natasha Singh <dukedesi22@gmail.com>
2023-01-10 13:39:20 -0500

    :construction_worker: Run ci workflow for 1 pkg not multiple

--- scripts/collect_packages.py
+++ scripts/collect_packages.py
@@ -12,25 +12,23 @@ RUN_FILEPATH = os.path.join(ROOT_DIR, "run.yaml")
 PACKAGES_DIR = "d3b_ingest_packages/packages"
 
 
-def collect_packages():
+def get_package():
     """
-    Read relative ingest package paths from run.yaml
-    Check to make sure each path exists
-    Output list of package paths
+    Read relative ingest package path from run.yaml
+    Check to make sure the path exists
+    Output package path
     """
 
     with open(RUN_FILEPATH) as f:
         data = yaml.load(f, Loader=SafeLoader)
 
-        paths = []
-        for p in data["packages"]:
-            new_path = os.path.join(PACKAGES_DIR, p)
-            # Check if path exists
-            os.stat(new_path)
-            # Write relative ingest package path to stdout
-            paths.append(p)
-        print(json.dumps(paths))
+        rel_path = data["package"]
+        new_path = os.path.join(PACKAGES_DIR, rel_path)
+        # Check if path exists
+        os.stat(new_path)
+        # write relative ingest package path to stdout
+        print(rel_path)
 
 
 if __name__ == "__main__":
-    collect_packages()
+    get_package()
